423eea7546ceee32e5f5379d8c53a7d2023a4f2b
GGANGTONG <qkrds0914@gmail.com>
2024-01-23 14:58:38 +0900

    제출용

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -1,34 +1,35 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: false,
+      enum: ['FOR_SALE', 'SOLD_OUT'],
+      default: 'FOR_SALE',
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    order: {
+      type: Number,
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: false,
-  },
-  status: {
-    type: String,
-    required: false,
-  },
-  date: {
-    type: Date,
-    required: false,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  order: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model('Product', productSchema);
